76ea284c2b0f332ca797fae60af96f6bd71ea1f9
David Bogner <davidbogner@gmail.com>
2020-04-21 17:43:52 +0200

    Generate plugin skeleton

---
+++ lang/en/mooduell.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_mooduell
+ * @category    string
+ * @copyright   2020 David Bogner <david.bogner@wunderbyte.at>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['messageprovider:status'] = 'Game status changes';
+$string['mooduell:managemooduellsettings'] = 'Manage the settings of a MooDuell game';
+$string['mooduell:viewstatistics'] = 'View game statistics';
+$string['mooduell:viewstudentnames'] = 'View full names of participants';
+$string['pluginname'] = 'MooDuell';
+$string['modulenameplural'] = 'MooDuells';
+$string['mooduellname'] = 'Name of the MooDuell game';
+$string['mooduellname_help'] = 'Choose a name for your game';
+$string['mooduellsettings'] = 'MooDuell settings';
+$string['modulename'] = 'MooDuell';
+$string['modulename'] = 'MooDuell';
+$string['modulename'] = 'MooDuell';
---
+++ lib.php
@@ -0,0 +1,99 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Library of interface functions and constants.
+ *
+ * @package     mod_mooduell
+ * @copyright   2020 David Bogner <david.bogner@wunderbyte.at>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Return if the plugin supports $feature.
+ *
+ * @param string $feature Constant representing the feature.
+ * @return true | null True if the feature is supported, null otherwise.
+ */
+function mooduell_supports($feature) {
+    switch ($feature) {
+        case FEATURE_MOD_INTRO:
+            return true;
+        default:
+            return null;
+    }
+}
+
+/**
+ * Saves a new instance of the mod_mooduell into the database.
+ *
+ * Given an object containing all the necessary data, (defined by the form
+ * in mod_form.php) this function will create a new instance and return the id
+ * number of the instance.
+ *
+ * @param object $moduleinstance An object from the form.
+ * @param mod_mooduell_mod_form $mform The form.
+ * @return int The id of the newly inserted record.
+ */
+function mooduell_add_instance($moduleinstance, $mform = null) {
+    global $DB;
+
+    $moduleinstance->timecreated = time();
+
+    $id = $DB->insert_record('mooduell', $moduleinstance);
+
+    return $id;
+}
+
+/**
+ * Updates an instance of the mod_mooduell in the database.
+ *
+ * Given an object containing all the necessary data (defined in mod_form.php),
+ * this function will update an existing instance with new data.
+ *
+ * @param object $moduleinstance An object from the form in mod_form.php.
+ * @param mod_mooduell_mod_form $mform The form.
+ * @return bool True if successful, false otherwise.
+ */
+function mooduell_update_instance($moduleinstance, $mform = null) {
+    global $DB;
+
+    $moduleinstance->timemodified = time();
+    $moduleinstance->id = $moduleinstance->instance;
+
+    return $DB->update_record('mooduell', $moduleinstance);
+}
+
+/**
+ * Removes an instance of the mod_mooduell from the database.
+ *
+ * @param int $id Id of the module instance.
+ * @return bool True if successful, false on failure.
+ */
+function mooduell_delete_instance($id) {
+    global $DB;
+
+    $exists = $DB->get_record('mooduell', array('id' => $id));
+    if (!$exists) {
+        return false;
+    }
+
+    $DB->delete_records('mooduell', array('id' => $id));
+
+    return true;
+}
