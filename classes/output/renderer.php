dd2860065bd5064e4199efed0444529f59016a3a
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2021-11-03 16:48:55 +0100

    Fix merge errors.

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -65,17 +65,6 @@ class renderer extends plugin_renderer_base {
      * @param templatable $question
      * @return void
      */
-    public function render_list_image(templatable $question) {
-        $data = $question->export_for_template($this);
-        return $this->render_from_template('mod_mooduell/list_image', $data);
-    }
-
-    /**
-     * render image of question in list.
-     *
-     * @param templatable $question
-     * @return void
-     */
     public function render_list_id(templatable $question) {
         $data = $question->export_for_template($this);
         return $this->render_from_template('mod_mooduell/list_id', $data);
@@ -115,17 +104,7 @@ class renderer extends plugin_renderer_base {
     }
 
     /**
-     * Render a mooduell view page.
-     * @param templatable $overview
-     * @return string|boolean
-     */
-    public function render_overview_teachers(templatable $overview) {
-        $data = $overview->export_for_template($this);
-        return $this->render_from_template('mod_mooduell/overview_teachers', $data);
-    }
-
-    /**
-     * render image of question in list.
+     * Render image of question in list.
      *
      * @param templatable $question
      * @return void
@@ -133,39 +112,6 @@ class renderer extends plugin_renderer_base {
     public function render_list_image(templatable $question) {
         $data = $question->export_for_template($this);
         return $this->render_from_template('mod_mooduell/list_image', $data);
-    }
-
-    /**
-     * render image of question in list.
-     *
-     * @param templatable $question
-     * @return void
-     */
-    public function render_list_id(templatable $question) {
-        $data = $question->export_for_template($this);
-        return $this->render_from_template('mod_mooduell/list_id', $data);
-    }
-
-    /**
-     * render image of question in list.
-     *
-     * @param templatable $question
-     * @return void
-     */
-    public function render_list_warnings(templatable $question) {
-        $data = $question->export_for_template($this);
-        return $this->render_from_template('mod_mooduell/list_warnings', $data);
-    }
-
-    /**
-     * render text & answers of question in list.
-     *
-     * @param templatable $question
-     * @return void
-     */
-    public function render_list_text(templatable $question) {
-        $data = $question->export_for_template($this);
-        return $this->render_from_template('mod_mooduell/list_text', $data);
     }
 
     /**
