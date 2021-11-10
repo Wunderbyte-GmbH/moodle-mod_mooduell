ea1faed05c37d6aa21cfb8f5600e14f577c217c0
georgmaisser <georg.maisser@wunderbyte.at>
2021-11-10 20:44:51 +0100

    Version 0.9.4 (2021111000)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mooduell';
-$plugin->release = 'v0.9.3';
-$plugin->version = 2021110204;
+$plugin->release = 'v0.9.4';
+$plugin->version = 2021111000;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_STABLE;
