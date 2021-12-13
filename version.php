9d64e7c708c256a22edd4d6cd2843e1d917aac6d
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2021-12-13 09:40:59 +0100

    Version 1.0.3 (2021121300)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mooduell';
-$plugin->release = 'v1.0.2';
-$plugin->version = 2021120600;
+$plugin->release = 'v1.0.3';
+$plugin->version = 2021121300;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_STABLE;
