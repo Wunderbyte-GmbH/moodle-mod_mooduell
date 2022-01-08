a8056ae1708ea12a4f1df2a31fc8dbfbff5b4643
georgmaisser <georg.maisser@wunderbyte.at>
2022-01-08 13:32:13 +0100

    Version 1.0.4 (2022010800)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mooduell';
-$plugin->release = 'v1.0.3';
-$plugin->version = 2021121300;
+$plugin->release = 'v1.0.4';
+$plugin->version = 2022010800;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_STABLE;
