c31b7674c5792b70bd7ca28c36fceffd69cf0795
cbadusch <fubbali@gmx.at>
2022-08-16 15:34:45 +0200

    Version Up

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mooduell';
 $plugin->release = 'v2.0.0';
-$plugin->version = 2022062002;
+$plugin->version = 2022062003;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_STABLE;
