d08e320b8336adb8c66a86465bd2831e7abc35d1
cbadusch <hello@christianbadusch.codes>
2023-08-30 14:37:27 +0200

    Fix cast error in qr plugin

--- thirdparty/vendor/bacon/bacon-qr-code/src/BaconQrCode/Encoder/MaskUtil.php
+++ thirdparty/vendor/bacon/bacon-qr-code/src/BaconQrCode/Encoder/MaskUtil.php
@@ -220,7 +220,7 @@ class MaskUtil
                 break;
 
             case 4:
-                $intermediate = (BitUtils::unsignedRightShift($y, 1) + ($x / 3)) & 0x1;
+                $intermediate = (BitUtils::unsignedRightShift($y, 1) + (int) ($x / 3)) & 0x1;
                 break;
 
             case 5:
