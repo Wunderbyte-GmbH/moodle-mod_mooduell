70e5f9f62546244749e6ae9aadebd6a3c65beb20
Georg Maißer <georg.maisser@wunderbyte.at>
2026-02-04 14:53:52 +0100

    Version (2026020400)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_mooduell';
 $plugin->release = 'v2.2.3';
 $plugin->supported = [404, 500];
-$plugin->version = 2025092500;
+$plugin->version = 2026020400;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
