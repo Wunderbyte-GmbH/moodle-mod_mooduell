4edc761a1f81f44bb01775b77997372188d51f7d
cbadusch <hello@christianbadusch.codes>
2026-02-09 09:56:58 +0100

    Version 2.2.5 (2026020900)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mooduell';
-$plugin->release = 'v2.2.4';
-$plugin->supported = [404, 501];
-$plugin->version = 2026020401;
+$plugin->release = 'v2.2.5';
+$plugin->supported = [405, 501];
+$plugin->version = 2026020900;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
